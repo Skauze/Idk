6d7c7399be7a763bf49797ed538210dc5f7c8a03
sonicrules11 <sonic886310@gmail.com>
2018-06-07 14:41:00 -0700

    Update

--- js/java.js
+++ js/java.js
@@ -1,6 +1,9 @@
 <script>
 // Get the video
 var video = document.getElementById("Background");
+// Get the audio
+var audio = document.getElementById("Music");
+audio.controls = false;
 // Get the button
 //var btn = document.getElementById("myBtn");
 </script>
